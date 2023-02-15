a5da0142c66ed8c2ac950aa10e3d2037cf28a915
Witold Zakrzewski <witoldzakrzewski@gmail.com>
2023-02-15 09:30:30 +0100

    19.1 ADDED findById method

---
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -0,0 +1,47 @@
+package com.crud.tasks.controller;
+
+import com.crud.tasks.domain.Task;
+import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/tasks")
+@RequiredArgsConstructor
+public class TaskController {
+
+    private final DbService service;
+    private final TaskMapper taskMapper;
+
+    @GetMapping
+    public List<TaskDto> getTasks() {
+        List<Task>tasks = service.getAllTask();
+        return taskMapper.mapToTaskDtoList(tasks);
+    }
+
+    @GetMapping(value = "{taskId}")
+    public TaskDto getTask(@PathVariable Long taskId) throws TaskNotFoundException {
+        return  taskMapper.mapToTaskDto(service.getTask(taskId));
+    }
+
+    @DeleteMapping
+    public void deleteTask(Long taskId) {
+
+    }
+
+    @PutMapping
+    public TaskDto updateTask( TaskDto taskDto) {
+        return new TaskDto(1L, "Edited test title", "Test content");
+
+
+    }
+
+    @PostMapping
+    public void createTask(TaskDto taskDto) {
+
+    }
+}
