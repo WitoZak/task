3bdc3e131cdeeee20b166effb9579fe61f1ea4bf
WitoZak <113088417+WitoZak@users.noreply.github.com>
2023-05-18 08:37:28 +0200

    32.2 Added Thymeleaf Config Template

--- src/main/java/com/crud/tasks/config/AdminConfig.java
+++ src/main/java/com/crud/tasks/config/AdminConfig.java
@@ -1,14 +1,21 @@
 package com.crud.tasks.config;
 
 import lombok.Getter;
-import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
-@Setter
 @Getter
 public class AdminConfig {
     @Value("${admin.mail}")
     private String adminMail;
+
+    @Value("${admin.name}")
+    private String adminName;
+
+    @Value("${info.company.name}")
+    private String companyDetail;
+
+    @Value("${goodbye}")
+    private String goodbye;
 }
